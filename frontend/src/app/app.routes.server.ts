a8293e7509611bc34a6c366a14430e131f899775
alexanderwilkins <alvanwil@gmail.com>
2025-01-31 09:38:12 -0700

    update the serve side rendered pages

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'year/:year',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'year/:year/set/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { RenderMode, ServerRoute } from '@angular/ssr';
 import { HomeComponent } from './home/home.component';
 import { SetDetailComponent } from './components/set-detail/set-detail.component';
 
